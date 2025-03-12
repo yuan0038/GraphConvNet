02b3ead7c875a2774222dd686c7f937ae3a12a12
yuan0038 <68579138+yuan0038@users.noreply.github.com>
2025-03-12 20:53:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 node_num=8  # the num gpu you want to use
-batch_size=256  #per gpu
+batch_size=128  #per gpu
 imagenet_path='/home/guest/workplace/lzy/imagenet'  #Relace it to your own imagenet path 
 model='graphconvnet_ti'   # Change to the model you want to train:graphconvnet_ti graphconvnet_s  graphconvnetp_ti graphconvnetp_s
 
 ####### Train Command######:Replace  'your/imagenet/root/path' to your own ImageNet path
-python -u -m torch.distributed.launch --master_port 22577 --nproc_per_node=${node_num} train.py  --data ${imagenet_path} --model  ${model}  --lr 4e-3   --output ./output/${model}_${batch_size}x${node_num}   >./${model}_${batch_size}x${node_num}.log 2>&1 &
+python -u -m torch.distributed.launch --master_port 22577 --nproc_per_node=${node_num} train.py  --data ${imagenet_path} --model  ${model}  --lr 2e-3   --output ./output/${model}_${batch_size}x${node_num}   >./${model}_${batch_size}x${node_num}.log 2>&1 &
 
 
 ###### resume    #######
